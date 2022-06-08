106eee5963c4f13d383ad579281639293567e7d3
RyuuseiKang <ryuuseikang@naver.com>
2022-06-08 21:16:15 +0900

    feature: ユーザモデル作成、ポスト作成

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """
+    ユーザー
+    """
+
+    handle = models.CharField(max_length=32, null=False)
+    profile_image_url = models.TextField(blank=False, null=True)
+
+    @property
+    def post_count(self) -> int:
+        """
+        このユーザーの投稿数
+        """
+        return self.posts.count()
